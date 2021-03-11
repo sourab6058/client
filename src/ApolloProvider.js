119d5d953b03a4147de8910cd127d5f6a93478a9
sourab6058 <sourab1236kumar@gmail.com>
2021-03-11 10:33:26 +0530

    package and auth editted

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://safe-waters-22999.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
